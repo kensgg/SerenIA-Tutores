9481e24dd0cd28842261f9626068b2413abcbf69
kensgg <g03.gudicruz@gmail.com>
2025-09-14 15:53:23 -0600

    second commit

--- app/services/firebase_service.py
+++ app/services/firebase_service.py
@@ -1,27 +1,34 @@
 import os
-import json
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, firestore
+
+# Carga las variables de entorno desde el archivo .env
+load_dotenv()
 
 def initialize_firebase():
     """
     Initializes Firebase and returns the Firestore client.
-    
-    Returns:
-        firestore.Client: The Firestore client instance.
     """
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(current_dir, '..', 'config', 'firebase_config.json')
+    firebase_config = {
+        "type": os.getenv("FIREBASE_TYPE"),
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+        "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
+    }
 
     try:
-        with open(config_path, 'r') as f:
-            config = json.load(f)
-
         if not firebase_admin._apps:
-            cred = credentials.Certificate(config)
+            cred = credentials.Certificate(firebase_config)
             firebase_admin.initialize_app(cred)
             print("Firebase initialized successfully!")
-        
         db = firestore.client()
         return db
     except Exception as e:
